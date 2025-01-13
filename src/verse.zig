d34e9c23c510c3298a878a52343c17f07e20cc99
Gregory Mullen <_@gr.ht>
2025-01-13 10:11:35 -0800

    reorder and clean up verse.zig

--- src/verse.zig
+++ src/verse.zig
@@ -1,33 +1,31 @@
 //! Verse: The framework.
 //!
-pub const RequestData = @import("request-data.zig");
-const cookies = @import("cookies.zig");
+pub const auth = @import("auth.zig");
 pub const template = @import("template.zig");
-const errors = @import("errors.zig");
-const endpoint = @import("endpoint.zig");
 
-pub const Frame = @import("frame.zig");
-pub const Server = @import("server.zig");
-pub const Router = @import("router.zig");
-pub const Request = @import("request.zig");
 pub const ContentType = @import("content-type.zig");
+pub const Frame = @import("frame.zig");
 pub const Headers = @import("headers.zig");
+pub const Request = @import("request.zig");
+pub const RequestData = @import("request-data.zig");
+pub const Router = @import("router.zig");
+pub const Server = @import("server.zig");
 
+const cookies = @import("cookies.zig");
 pub const Cookie = cookies.Cookie;
 
 pub const fileOnDisk = @import("static-file.zig").fileOnDisk;
 
+const endpoint = @import("endpoint.zig");
 pub const Endpoints = endpoint.Endpoints;
 
+const errors = @import("errors.zig");
 pub const RoutingError = Router.RoutingError;
 pub const ServerError = errors.ServerError;
 pub const ClientError = errors.ClientError;
 pub const NetworkError = errors.NetworkError;
 pub const Error = errors.Error;
 
-pub const auth = @import("auth.zig");
-
 test "verse" {
     @import("std").testing.refAllDecls(@This());
-    @import("std").testing.refAllDecls(Frame);
 }
