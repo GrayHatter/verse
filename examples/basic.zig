0d57619ca5e391051e73be9241eb0e50bbee3ffb
Gregory Mullen <_@gr.ht>
2025-01-14 10:00:29 -0800

    add some docs to basic example

--- examples/basic.zig
+++ examples/basic.zig
@@ -1,10 +1,18 @@
 //! The quickest way to start a verse server
-//!
 
+/// Step 0: Create your endpoint function
+fn index(frame: *verse.Frame) Router.Error!void {
+    try frame.sendHTML(.ok, "Hello World!");
+}
+
+/// Step 1: Add your endpoint as a route
 const routes = Router.Routes(&[_]Router.Match{
+    // index should to resolve to the root, so use an empty name ""
     Router.GET("", index),
 });
 
+/// Step 2: Write main, and start a verse server.
+/// That's it! Verse listens on http://localhost:8080 by default.
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -20,11 +28,6 @@ pub fn main() !void {
     };
 }
 
-fn index(frame: *verse.Frame) Router.Error!void {
-    try frame.sendHTML(.ok, "hello world");
-}
-
 const std = @import("std");
 const verse = @import("verse");
 const Router = verse.Router;
-const BuildFn = Router.BuildFn;
