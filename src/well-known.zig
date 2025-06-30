a33eb3a8d12989dd85390328a3d37a49c20a9cac
Gregory Mullen <_@gr.ht>
2025-06-29 21:18:06 -0700

    add well-known.zig
    this isn't wired up yet, but has been sitting in my src tree
    waiting for a mistake for too long

---
+++ src/well-known.zig
@@ -0,0 +1,28 @@
+pub const Endpoint = struct {
+    pub const verse_name = .@".well-known";
+
+    pub const verse_routes = [_]Router.Match{
+        Router.GET("atproto-did", atprotoDid),
+        Router.GET("security.txt", security),
+        Router.GET("traffic-advice", trafficAdvice),
+    };
+
+    fn atprotoDid(f: *Frame) Router.Error!void {
+        return f.sendDefaultErrorPage(.not_found);
+    }
+
+    fn security(f: *Frame) Router.Error!void {
+        f.status = .ok;
+        f.content_type = .text;
+        f.sendHeaders();
+        f.sendRawSlice("security.txt is not provided for this site.");
+    }
+
+    fn trafficAdvice(f: *Frame) Router.Error!void {
+        return f.sendDefaultErrorPage(.not_found);
+    }
+
+    const std = @import("std");
+    const Frame = @import("frame.zig");
+    const Router = @import("router.zig");
+};
