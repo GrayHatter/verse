d704896777f5cd92f88d0dbfa9316bb78c20e5b0
Gregory Mullen <_@gr.ht>
2026-04-15 12:25:05 -0700

    add cleaned to html abx
    eventually this aspect should live in abx not abx.html

--- src/antibiotic/Html.zig
+++ src/antibiotic/Html.zig
@@ -1,4 +1,5 @@
 text: []const u8,
+text_cleaned: []const u8 = &.{},
 
 const Html = @This();
 
@@ -6,17 +7,23 @@ pub fn abx(str: []const u8) Html {
     return .{ .text = str };
 }
 
-pub fn format(self: Html, out: *Writer) error{WriteFailed}!void {
-    for (self.text) |c| {
-        try clean(c, out);
+pub fn cleaned(str: []const u8) Html {
+    return .{ .text_cleaned = str, .text = &.{} };
+}
+
+pub fn format(html: Html, w: *Writer) error{WriteFailed}!void {
+    if (html.text_cleaned.len >= html.text.len) {
+        return w.writeAll(html.text_cleaned);
+    } else for (html.text) |c| {
+        try clean(c, w);
     }
 }
 
 /// Basic html sanitizer. Will replace all chars, even when it may be
 /// unnecessary to do so in context.
-pub fn clean(in: u8, out: *Writer) error{WriteFailed}!void {
+pub fn clean(in: u8, w: *Writer) error{WriteFailed}!void {
     var same = [1:0]u8{in};
-    _ = try out.writeAll(switch (in) {
+    _ = try w.writeAll(switch (in) {
         '<' => "&lt;",
         '&' => "&amp;",
         '>' => "&gt;",
@@ -28,10 +35,10 @@ pub fn clean(in: u8, out: *Writer) error{WriteFailed}!void {
 
 test Html {
     var a = std.testing.allocator;
-    const cleaned = try std.fmt.allocPrint(a, "{f}", .{Html{ .text = "<tags not allowed>" }});
-    defer a.free(cleaned);
+    const cleaned_text = try std.fmt.allocPrint(a, "{f}", .{Html{ .text = "<tags not allowed>" }});
+    defer a.free(cleaned_text);
 
-    try std.testing.expectEqualStrings("&lt;tags not allowed&gt;", cleaned);
+    try std.testing.expectEqualStrings("&lt;tags not allowed&gt;", cleaned_text);
 }
 
 const std = @import("std");
